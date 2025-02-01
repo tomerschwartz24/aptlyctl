e44aa2f522e030a8fc3f58219ae80789d2febff2
Tomer Schwartz <tomerschwartz2411@gmail.com>
2025-02-01 17:27:31 +0200

    Add 404 error handling for repo show

--- cli.py
+++ cli.py
@@ -5,12 +5,18 @@ import requests
 import os 
 
 API_USER = os.environ.get('API_USER')
-API_PASS = os.environ.get('API_PASSWORD')
+API_PASS = os.environ.get('API_PASS')
 GPG_PASSPHRASE = os.environ.get('GPG_PASSPHRASE')
 
 
 def repo_show(repo_name):
-    response = requests.get('{}{}{}'.format(APTLY_URL, routes["repo"], repo_name), auth=(API_USER, API_PASS))
-    repo_data = response.text
-    return repo_data
+    # import ipdb
+    # ipdb.set_trace()
+    try:
+        response = requests.get('{}{}{}'.format(APTLY_URL, routes["repo"], repo_name), auth=(API_USER, API_PASS))
+        response.raise_for_status()
+        repo_data = response.text
+        return repo_data
 
+    except requests.exceptions.HTTPError as error:
+        return error
